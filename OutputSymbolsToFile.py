fffb54bc78b34af78ed6e893eb6e6ebead5bd081
Ajgiz_Mason <ajgizmavletberdin@gmail.com>
2025-06-24 02:03:39 +0500

    Finished the main part. It remains to figure out the 3rd task(

--- OutputSymbolsToFile.py
+++ OutputSymbolsToFile.py
@@ -8,7 +8,7 @@ with open('Mas0n.txt', 'w', encoding='utf-8') as file:
         '\u0041\u0079\u0067\u0069\u007A \u004D\u0061\u0076\u006C\u0065\u0074\u0062\u0065\u0072\u0064\u0069\u006E \t  EN_From_UTF-16_LE\n')
     #Mavletberdin_And_Ajgiz_From_ASCII
     file.write(
-        chr(65) + chr(121) + chr(103) + chr(105) + chr(122) + ' ' +
+        chr(65) + chr(121) + chr(103) + chr(105)  + chr(122) + ' ' +
         chr(77) + chr(97) + chr(118) + chr(108) + chr(101) + chr(116) +
         chr(98) + chr(101) + chr(114) + chr(100) + chr(105) + chr(110) +
         ' \t  EN_From_ASCII\n'
