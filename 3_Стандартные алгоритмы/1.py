09db5fc0931726c033182c384e362b414e6babb8
Ajgiz_Mason <ajgizmavletberdin@gmail.com>
2025-06-24 22:51:23 +0500

    1.2

--- 3_Стандартные алгоритмы/1.py
+++ 3_Стандартные алгоритмы/1.py
@@ -1,3 +1,4 @@
+
 PriceOfNotebook=2.75
 PriceOfPencil=0.85
 #Счет
@@ -8,13 +9,52 @@ print("Цена карандаша (руб.):  0.85")
 print("Количество карандашей (шт): ",end="")
 CountOfPencil=int(input())
 SummOfPurchase = PriceOfPencil*CountOfPencil + PriceOfNotebook*CountOfNotebook
-print("\n\tСтоимость покупки: ",SummOfPurchase)
+if (300<SummOfPurchase < 500.0):
+    Discount = 0.03
+    SummOfPurchaseWithDiscount=SummOfPurchase * (1-Discount)
+elif (500.0<SummOfPurchase < 1000.0):
+    Discount = 0.05
+    SummOfPurchaseWithDiscount=SummOfPurchase * (1-Discount)
+elif (SummOfPurchase > 1000.0):
+    Discount = 0.10
+    SummOfPurchaseWithDiscount=SummOfPurchase * (1-Discount)
+else :
+    Discount = 0.0
+    SummOfPurchaseWithDiscount = SummOfPurchase * (1 - Discount)
+
+
+print("\n\tСтоимость покупки: {0} руб.".format(SummOfPurchase))
+print("\tСКИДКА: {0}%".format(int(Discount)))
+print("\tИТОГ: {0} руб.\t".format(SummOfPurchaseWithDiscount))
 
 #Чек
 print("\n\n\n\nВАШ ЧЕК")
-print("Дата 08.12.2006")
-print("Кассир Ахметшина А.И\n")
+print("Дата 14.04.2035")
+print("Кассир Мавлетбердина А.А.\n")
 print("Тетрадь	{0} шт.* {1}		{2}".format(PriceOfNotebook,CountOfNotebook,PriceOfNotebook*CountOfNotebook))
 print("Карандаш	{0} шт.* {1}		{2}\n".format(PriceOfPencil,CountOfPencil,PriceOfPencil*CountOfPencil))
-print("ИТОГО К ОПЛАТЕ			{0}.".format(SummOfPurchase))
+print("\tСтоимость покупки: {0} руб.".format(SummOfPurchase))
+print("\tСкидка	{0}% : {1} ".format(int(Discount),SummOfPurchase*Discount))
+print("\tИТОГО К ОПЛАТЕ   {0} руб.".format(SummOfPurchase))
 
+# Цена тетради (руб.):  2.75
+# Количество тетрадей (шт.): 150
+# Цена карандаша (руб.): 0.85
+# Количество карандашей (шт): 25
+#
+# 	Стоимость покупки: 433.75 руб.
+# 	СКИДКА 3%:	13.01
+# 	ИТОГ:	420.74 руб.
+#
+#  Предусмотрите печать чека в файл в виде:
+# Дата 30.06.2015
+# Кассир Иванова И.П.
+#
+# Тетрадь	150 шт.* 2.75		412.5
+# Карандаш	25 шт.* 0.85		21.25
+#
+#
+# Стоимость покупки: 433.75 руб.
+# 	СКИДКА 3%:	13.01
+#
+# ИТОГО К ОПЛАТЕ	420.74 руб.
