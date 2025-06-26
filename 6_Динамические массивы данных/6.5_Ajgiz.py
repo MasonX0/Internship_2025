afec23d70481fc2c33fe997c284b09cf99fc4aa9
Tyr <ajgizmavletberdin@gmail.com>
2025-06-27 01:24:28 +0500

    little corrections, it remains to update the DOCX file

--- 6_Динамические массивы данных/6.5_Ajgiz.py
+++ 6_Динамические массивы данных/6.5_Ajgiz.py
@@ -1,39 +1,36 @@
 import numpy as np
-fs=[]
-Ls = []
-Itog = []
-Ls1 = []
-LsRooms = []
-LsBuild = []
-LsFlours = []
-choice = int(input("1 - ввод с клавиатуры, 2 - автозаполнение, 3 - с файла : "))
+Final=[]
+ListOfBuildings = []
+ListOfFloors = []
+List=[]
+choice = int(input("1 - ввод с клавиатуры, 2 - с файла : "))
 if choice == 1:
-    List = input("Введите как в примере 1_5_23 ч\з пробел:")
-    List1 = List.split(" ")
-    for i in List1:
-        Ls.append(i.split("_"))
-        Ls1.append(i.split("_")[:-1])
-        LsRooms.append(i.split("_")[-1])
-        LsFlours.append(i.split("_")[-2])
-        if (i.split("_")[0]) not in LsBuild: LsBuild.append(i.split("_")[0])
+    line = input("Введите как в примере 1_5_23 ч\з пробел:")
+    incomplitedlist = line.split(" ")
+if choice == 2:
+    with open("DataOfRooms.txt", 'r', encoding='utf-8') as file:
+        line = file.readline()
+    incomplitedlist = list(line.split(" "))
 
-
-for i in LsBuild:
-    sffl = 0
-    for g in LsFlours:
-        sfl = 0
-        for j in Ls[1:]:
+for i in incomplitedlist:
+    List.append(i.split("_"))
+    ListOfFloors.append(i.split("_")[-2])
+    if (i.split("_")[0]) not in ListOfBuildings: ListOfBuildings.append(i.split("_")[0])
+for i in ListOfBuildings:
+    maxinBuilding = 0
+    for g in ListOfFloors:
+        infloor = 0
+        for j in List[1:]:
             if i == j[0] and g == j[-2]:
-                sfl+=int(j[-1]) #Нашел сумму комнат на этаже конкр общаги
-        if sfl>sffl:
-            sffl=sfl
-    Itog.append([int(i),sffl])#Добавляю в список этаж с наиб зас-ем в этой общаге
-maxc = Itog[0][-1]
-numberofmax = Itog[0][0]
-for i in Itog[1:]:
+                infloor += int(j[-1])
+        if infloor > maxinBuilding:
+            maxinBuilding = infloor
+    Final.append([int(i), maxinBuilding])
+maxc = Final[0][-1]
+numberofmax = Final[0][0]
+for i in Final[1:]:
     if i[-1] > maxc:
         maxc = i[-1]
         numberofmax = i[0]
         print(i)
-print("Саммый заселенный этаж находится в  {}  общежитии, там живет  {}  человек.".format(numberofmax,maxc))
-
+print("Самый заселенный этаж находится в  {}  общежитии, там живет  {}  человек.".format(numberofmax, maxc))
